086e62f37692c96291a286ef879935d536b909a1
okdoittttt <sonokmoo@gmail.com>
2024-09-17 20:33:35 +0900

    REFACTOR: 로봇 리스트 출력 화면 디자인 변경

--- app/robot_arm_controller/lib/pages/robotList/robotsconnectionScreen.dart
+++ app/robot_arm_controller/lib/pages/robotList/robotsconnectionScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:robot_arm_controller/item/loginItems/logoutButton.dart';
 import 'package:robot_arm_controller/item/robotListItems/robotsList.dart';
@@ -13,21 +15,184 @@ class _RobotsconnectionScreenState extends State<RobotsconnectionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        title: Text('Robot List'),
-      ),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView( // 스크롤 가능한 영역을 만듭니다.
+      body: SingleChildScrollView(
         child: Column(
+          mainAxisSize: MainAxisSize.max,
           children: [
-            Align(
-              alignment: AlignmentDirectional.topEnd,
-              child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 20, 20, 0),
-                child: LogoutButton(),
-              ),
+            Stack(
+              children: [
+                ClipRect(
+                  child: ImageFiltered(
+                    imageFilter: ImageFilter.blur(
+                      sigmaX: 2,
+                      sigmaY: 2,
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      height: 370,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 370,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Color(0xFF1E2429), Color(0x001E2429)],
+                      stops: [0, 1],
+                      begin: AlignmentDirectional(0, 0),
+                      end: AlignmentDirectional(0, -1),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 24),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Padding(
+                              padding:
+                              EdgeInsetsDirectional.fromSTEB(24, 0, 24, 8),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'DUMMY',
+                                    style: TextStyle(color: Colors.white, fontSize: 48),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                              EdgeInsetsDirectional.fromSTEB(24, 0, 24, 8),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Welcome!',
+                                    style: TextStyle(color: Colors.white, fontSize: 25),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                              EdgeInsetsDirectional.fromSTEB(24, 0, 24, 8),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Select Robots',
+                                    style: TextStyle(color: Colors.white, fontSize: 25),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                              EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+                              child: Container(
+                                width: double.infinity,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                alignment: AlignmentDirectional(0, 0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Expanded(
+                                      child: Padding(
+                                        padding: EdgeInsetsDirectional.fromSTEB(
+                                            4, 0, 4, 0),
+                                        child: TextFormField(
+                                          obscureText: false,
+                                          decoration: InputDecoration(
+                                            labelText:
+                                            'Robot name ...',
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color: Colors.white,
+                                                width: 2,
+                                              ),
+                                              borderRadius:
+                                              BorderRadius.circular(30),
+                                            ),
+                                            focusedBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color: Color(0x00000000),
+                                                width: 2,
+                                              ),
+                                              borderRadius:
+                                              BorderRadius.circular(30),
+                                            ),
+                                            errorBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color: Color(0x00000000),
+                                                width: 2,
+                                              ),
+                                              borderRadius:
+                                              BorderRadius.circular(30),
+                                            ),
+                                            focusedErrorBorder:
+                                            OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color: Color(0x00000000),
+                                                width: 2,
+                                              ),
+                                              borderRadius:
+                                              BorderRadius.circular(30),
+                                            ),
+                                            filled: true,
+                                            fillColor: Colors.white,
+                                            prefixIcon: Icon(
+                                                Icons.search_sharp,
+                                                color: Colors.orange
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.orange
+                                      ),
+                                      onPressed: () {},
+                                      child: Text('SEARCH', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                    ),)
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
             RobotsList(),
             RobotsList(),
