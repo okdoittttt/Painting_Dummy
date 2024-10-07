92ace3e93ebce4c9b86b091cd0ef9e47b9b9e2de
okdoittttt <sonokmoo@gmail.com>
2024-10-07 16:44:38 +0900

    feat: http 요청 처리 클래스

---
+++ app/robot_arm_controller/lib/pages/manualmode/ManualMoveControll.dart
@@ -0,0 +1,36 @@
+import 'package:http/http.dart' as http;
+
+class HttpService {
+  final String url;
+
+  HttpService(this.url);
+
+  Future<String> sendRequest() async {
+    try {
+      final uri = Uri.parse(url);
+      final response = await http.post(uri);
+      if (response.statusCode == 200) {
+        return '요청 성공!';
+      } else {
+        return '요청 실패: ${response.statusCode}';
+      }
+    } catch (e) {
+      return '에러 발생: $e';
+    }
+  }
+
+  Future<String> sendRequestStop() async {
+    try {
+      final uri = Uri.parse(url);
+      final response = await http.post(uri);
+      if (response.statusCode == 200) {
+        return '요청 성공!';
+      } else {
+        return '요청 실패: ${response.statusCode}';
+      }
+    } catch (e) {
+      return '에러 발생: $e';
+    }
+  }
+
+}
