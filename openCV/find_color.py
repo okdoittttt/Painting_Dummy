39a16f5d4310112a6d40730f76b0c7e1f427c405
4444ki <104543703+4444ki@users.noreply.github.com>
2024-10-27 16:53:56 +0900

    feat : 좌측 상단에 좌표 및 거리 표시
    중앙에 표시하는 거리를 좌측 상단으로 이동하고 거리를 나타낼 수 있도록함

--- openCV/find_color.py
+++ openCV/find_color.py
@@ -4,14 +4,14 @@ from util import get_limits
 
 red = [0, 0, 255]
 # 최소 컨투어 면적을 설정 (너무 작은 점을 무시)
-min_contour_area = 400  # 이 값을 조정하여 감지할 객체의 최소 크기를 설정하세요
+min_contour_area = 400    # 이 값을 조정하여 감지할 객체의 최소 크기를 설정하세요
 
 # Intel Realsense 카메라 초기화
 dc = DepthCamera()
 
 while True:
     # 카메라 프레임 읽기
-    ret, depth_frame, color_frame = dc.get_frame()
+    ret, depth_frame, color_frame, depth_info = dc.get_frame()
 
     # BGR에서 HSV로 변환
     hsvImage = cv2.cvtColor(color_frame, cv2.COLOR_BGR2HSV)
@@ -29,11 +29,11 @@ while True:
             M = cv2.moments(contour)
 
             # 무게중심 (centroid) 좌표 계산
-            if M['m00'] != 0:
-                center_x = int(M['m10'] / M['m00'])
-                center_y = int(M['m01'] / M['m00'])
-            else:
-                center_x, center_y = 0, 0
+            # if M['m00'] != 0:
+            #     center_x = int(M['m10'] / M['m00'])
+            #     center_y = int(M['m01'] / M['m00'])
+            # else:
+            #     center_x, center_y = 0, 0
 
             # 컨투어에서 사각형 경계 구하기
             x, y, w, h = cv2.boundingRect(contour)
@@ -42,21 +42,30 @@ while True:
             cv2.rectangle(color_frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
 
             # 중심 좌표 표시 (원의 중심에 점 찍기)
-            cv2.circle(color_frame, (center_x, center_y), 5, (255, 0, 0), -1)
+            # cv2.circle(color_frame, (center_x, center_y), 5, (255, 0, 0), -1)
 
+            # 좌표 표시 (왼쪽 상단에 점 찍기)
+            cv2.circle(color_frame, (x, y), 5, (255, 0, 0), -1)
             # 좌표 텍스트로 화면에 출력
             #distance = depth_frame[center_x, center_y]
-            cv2.putText(color_frame, f"Center: ({center_x}, {center_y})",
-                        (center_x - 50, center_y - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (255, 255, 255), 2)
+            # cv2.putText(color_frame, f"Center: ({center_x}, {center_y})",
+            #             (center_x - 50, center_y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+            #             0.5, (255, 255, 255), 2)
+
+            # 좌표 텍스트로 화면에 출력
+            distance = round(depth_info.get_distance(x,y) * 100)
+            cv2.putText(color_frame, f"Coordinate: ({x}, {y}) Distance: {distance}",
+                            (x - 50, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5, (255, 255, 255), 2)
 
 
     # 결과 화면에 출력
     cv2.imshow("Color frame", color_frame)
-    cv2.imshow("Depth frame", depth_frame)
+    #cv2.imshow("Depth frame", depth_frame)
     # ESC 키 입력 시 루프 종료
     key = cv2.waitKey(1)
     if key == 27:
+        dc.release()
         break
 
 # 리소스 해제
